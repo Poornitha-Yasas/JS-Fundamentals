38d8df75acef8087cf4708ae82a5223c7c1d1319
Poornitha-Yasas <poornithayasasx0@gmail.com>
2024-12-22 20:46:54 +0530

    variable

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -1,2 +1,27 @@
 //Hellow World
 console.log('Hello World');
+
+//Variables
+var firstName = "Poornitha"
+let lastName = "Yasas"
+console.log(lastName)
+
+var age, dateOfBirth, sex
+age = "23"
+sex = "male"
+console.log(age)
+age = "24"
+console.log(age)
+
+
+//Constants
+const Occupation = "Quality Assurance Engineer"
+console.log(Occupation)
+
+
+//Data Types
+var middleName = "Yasas"  //String
+var age = 23  //Number
+var isMarried = false  //Boolean
+var numbberOfChildren = null //Null
+var numberOfCars = undefined //Undefined
