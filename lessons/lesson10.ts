331aa929c9b5ebed4b75ddd562221e600ba69d2a
Poornitha-Yasas <poornithayasasx0@gmail.com>
2024-12-23 19:42:30 +0530

    Typescript

---
+++ lessons/lesson10.ts
@@ -0,0 +1,12 @@
+
+var customerFirstName: string = "Poornitha"
+var customerLastName: string = "Yasas"
+var customerAge: number = 23
+
+type Customer = {firstName: string, lastName: string, active: boolean}
+
+var firstCustomer: Customer = {
+    firstName: "Poornitha",
+    lastName: "Yasas",
+    active: true
+}
