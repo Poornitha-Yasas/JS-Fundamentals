f1e1ca0ca311853c2714667c562226a7b0e96128
Poornitha-Yasas <poornithayasasx0@gmail.com>
2024-12-23 19:37:41 +0530

    Methods

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -1,3 +1,20 @@
 export function printAge(age) {
     console.log(age);
 }
+
+export class CustomerDetails{
+
+    printFirstName(firstName){
+        console.log(firstName);
+    }
+
+    /**
+     * This method prints the last name
+     * @param {string} lastName
+     */
+    printLastName(lastName){
+        console.log(lastName);
+    }
+}
+
+export const customerDetails = new CustomerDetails()
