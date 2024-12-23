475a87bd7c905bbbd85c44d947bc53f33cffc88b
Poornitha-Yasas <poornithayasasx0@gmail.com>
2024-12-23 14:32:38 +0530

    Relational or Comparison Operators

---
+++ lessons/lesson4.js
@@ -0,0 +1,15 @@
+//Relational or Comparison Operators
+
+// >- More than
+// < - Less than
+// >= - More than equal
+// <= - Less than equal
+
+var result = 10 > 5
+console.log(result)
+
+//Equality Operators
+
+var x = 1
+console.log(x == '1')  //lose comparison
+console.log(x === '1') //strict comparison
