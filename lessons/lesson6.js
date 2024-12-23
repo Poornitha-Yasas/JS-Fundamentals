ed11735b0c0464e689a88a430af49b9139b0282d
Poornitha-Yasas <poornithayasasx0@gmail.com>
2024-12-23 18:04:20 +0530

    Conditonal Statement

---
+++ lessons/lesson6.js
@@ -0,0 +1,21 @@
+//Conditonal Statement
+
+// if(condition){
+//     //execute code
+// }else{
+//     //execute code
+// }
+
+//If houe between 6 and 12: Good Morning!
+//If houe between 12 and 18: Good Afternoon!
+//Otherwise: Good Evening!
+
+var hour = 17
+
+if (hour >= 6 && hour < 12){
+    console.log('Good Morning!')
+}else if (hour >= 12 && hour < 18){
+    console.log('Good Afternoon!')
+}else{
+    console.log('Good Evening!')
+}
